7e01a792807bd3b9df1d55b6f023457132a02bd5
spooqr <martinmeyer451@gmail.com>
2021-05-13 17:06:19 -0300

    Added comments

--- LabCaja/src/caja/Main.java
+++ LabCaja/src/caja/Main.java
@@ -6,19 +6,19 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        Caja caja1 = new Caja();
+        Caja caja1 = new Caja(); // Nuevo objeto con constructor vacio
         caja1.ancho = 3;
         caja1.alto = 2;
         caja1.profundo = 6;
         caja1.calcularVolumen();
 
-        Caja caja2 = new Caja(3, 2, 6);
+        Caja caja2 = new Caja(3, 2, 6); // Nuevo con parametros
         caja2.calcularVolumen();
 
         Caja caja3 = new Caja(4, 5, 10);
         caja3.calcularVolumen();
 
-        Caja caja4 = new Caja();
+        Caja caja4 = new Caja(); // Nuevo objeto con atributos asignados desde la consola
         System.out.println("Ancho: ");
         caja4.ancho = Integer.parseInt(sc.nextLine());
         System.out.println("Alto: ");
